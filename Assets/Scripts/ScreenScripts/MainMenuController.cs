d9f91a6444821f581204a3f321e4799716f48f1c
Jessica <jessica.galvan@hotmail.com>
2021-05-01 18:15:39 -0300

    MainMenu
    Se completo el MenuPrincipal, falta solo cambiar el nombre de la escena del nivel y descomentar el load scene para que funcione

--- Assets/Scripts/ScreenScripts/MainMenuController.cs
+++ Assets/Scripts/ScreenScripts/MainMenuController.cs
@@ -1,18 +1,72 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("AllMenus Settings")]
+    [SerializeField] private GameObject mainMenu;
+    //[SerializeField] private GameObject helpMenu;
+    [SerializeField] private GameObject creditsMenu;
+
+    [Header("MainMenu Settings")]
+    [SerializeField] private Button playButton;
+    [SerializeField] private Button creditsButton;
+    [SerializeField] private Button exitButton;
+    private bool mainMenuCheck;
+    private string level;
+
+    [Header("Credits Settings")]
+    [SerializeField] private Button goBackCreditsButton;
+
+
     void Start()
     {
-        
+        playButton.onClick.AddListener(OnPlayHandler);
+        creditsButton.onClick.AddListener(OnCreditsHandler);
+        goBackCreditsButton.onClick.AddListener(OnGoBackHandler);
+        exitButton.onClick.AddListener(OnQuitHandler);
+
+        OnGoBackHandler(); //Si o si, aseguramos que inicie en el MenuInicial
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape) && !mainMenuCheck) //Esto es porque si no estan en uno de los sub menus, pueden volver para atras con Escape
+        {
+            OnGoBackHandler();
+        }
+    }
+
+
+    private void OnPlayHandler()
+    {
+        Debug.Log("Level");
+        //SceneManager.LoadScene(level);
+    }
+
+    private void OnCreditsHandler()
+    {
+        mainMenu.SetActive(false);
+        //helpMenu.SetActive(false);
+        creditsMenu.SetActive(true);
+        mainMenuCheck = false;
+    }
+
+    private void OnGoBackHandler()
+    {
+        mainMenu.SetActive(true);
+        //helpMenu.SetActive(false);
+        creditsMenu.SetActive(false);
+        mainMenuCheck = true;
+    }
+
+    private void OnQuitHandler()
+    {
+        Application.Quit();
+        Debug.Log("Se cierra el juego");
     }
 }
