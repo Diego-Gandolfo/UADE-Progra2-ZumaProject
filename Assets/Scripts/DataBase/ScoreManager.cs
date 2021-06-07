592bc066461679b11bb93369631997c1768fa503
Diego Gandolfo <Diego.Gandolfo@Gmail.com>
2021-06-07 06:50:45 -0300

    Implementacion provisoria del QuickSort sin Base de Datos

--- Assets/Scripts/DataBase/ScoreManager.cs
+++ Assets/Scripts/DataBase/ScoreManager.cs
@@ -1,15 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
     [SerializeField] private int level;
     private DBController database;
 
+    [SerializeField] private Text[] textNameUI;
+    [SerializeField] private Text[] textLevelUI;
+    [SerializeField] private Text[] textScoreUI;
+
     void Start()
     {
         database = DBController.Instance;
+    }
+
+    private void Awake() // TODO: el Awake es temporal para probar el QuickSort
+    {
+        // chequeamos que los tamaños de los 3 Arrays sean iguales
+        if (textNameUI.Length != textLevelUI.Length || textNameUI.Length != textScoreUI.Length)
+            Debug.LogError("Los tamaños de los Arrays del Canvas no coinciden");
+
+        var listPlayers = new List<Player>();
+
+        // llenamos la lista con player random
+        for (int i = 0; i < textNameUI.Length; i++)
+        {
+            var player = new Player();
+            listPlayers.Add(player);
+        }
+
+        // hacemos el QuickSort de la Lista y le indicamos cual es el primer y ultimo elemento que tiene que ordenar
+        QuickSort(listPlayers, 0, listPlayers.Count - 1);
+
+        // para ver si se cargo bien la lista
+        foreach (var player in listPlayers) 
+            print($"Name: {player.Name} / Level: {player.Level} / Score: {player.Score}");
+
+        // actualizamos los Text del Canvas
+        for (int i = 0; i < listPlayers.Count; i++)
+        {
+            var index = (listPlayers.Count - i) - 1;
+            textNameUI[i].text = listPlayers[index].Name;
+            textLevelUI[i].text = listPlayers[index].Level.ToString();
+            textScoreUI[i].text = listPlayers[index].Score.ToString();
+        }
     }
 
     private void Update()
@@ -61,9 +98,11 @@ public class ScoreManager : MonoBehaviour
     public void InsertRandomPlayerInRanking() //Genera un player random, lo inserta en player y luego en ranking
     {
         var player = new Player();
+
         database.InsertPlayer(player); //lo insertamos en la base
         player.Id = database.GetLastPlayerId(); //Obtenemos el id del ultimo player insertado 
         print($"{player.Id} {player.Name} {player.Level} {player.Score} {player.Time}");
+
         database.InsertRanking(player);
         var ranking = database.GetLatestRanking();
         print($"Ranking {ranking.Name}, Nivel: {ranking.Level} Score: {ranking.Score} Time: {ranking.Time}");
@@ -91,6 +130,54 @@ public class ScoreManager : MonoBehaviour
         foreach (var ranking in rankings)
         {
             print($"Ranking {ranking.Name}, Nivel: {ranking.Level} Score: {ranking.Score} Time: {ranking.Time}");
+        }
+    }
+
+    private int Partition(List<Player> arr, int left, int right)
+    {
+        int pivot;
+        int aux = (left + right) / 2;
+        pivot = arr[aux].Score;
+
+        while (true)
+        {
+            while (arr[left].Score < pivot)
+            {
+                left++;
+            }
+            while (arr[right].Score > pivot)
+            {
+                right--;
+            }
+            if (left < right)
+            {
+                Player temp = arr[right];
+                arr[right] = arr[left];
+                arr[left] = temp;
+            }
+            else
+            {
+                return right;
+            }
+        }
+    }
+
+    private void QuickSort(List<Player> arr, int left, int right)
+    {
+        int pivot;
+        if (left < right)
+        {
+            pivot = Partition(arr, left, right);
+            if (pivot > 1)
+            {
+                // mitad del lado izquierdo del vector
+                QuickSort(arr, left, pivot - 1);
+            }
+            if (pivot + 1 < right)
+            {
+                // mitad del lado derecho del vector
+                QuickSort(arr, pivot + 1, right);
+            }
         }
     }
 }
