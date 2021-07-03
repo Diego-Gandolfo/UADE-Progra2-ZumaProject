9192e4a1bb10a6b138d2b072b35a4b1b49495c2d
Jessica <jessica.galvan@hotmail.com>
2021-07-03 16:38:51 -0300

    Se borro comentario

--- Assets/Scripts/CanonScripts/DeathController.cs
+++ Assets/Scripts/CanonScripts/DeathController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
 
-public class DeathController : MonoBehaviour
+public class DeathController : MonoBehaviour //Necesitamos esto para setearle el color a la explosion y para decirle que se destruya luego de animarse. 
 {
     [SerializeField] private float timer1;
 
@@ -14,7 +14,7 @@ public class DeathController : MonoBehaviour
 
     void Update()
     {
-        if(Time.time > timer1)         //Primer timer, para instanciar luego de la animacion de muerte, la reward.
+        if(Time.time > timer1)
         {
             Destroy(gameObject);
         }
